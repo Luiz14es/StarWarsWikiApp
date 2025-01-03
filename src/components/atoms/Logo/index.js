a405cccba17b31552a5a46e31eea69445dccf00f
Luiz Eduardo <duduamaralspereira@gmail.com>
2025-01-02 21:39:42 -0300

    first commit

---
+++ src/components/atoms/Logo/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyledImage } from "./styles";
+
+
+export const Logo = () => {
+    return(
+      <StyledImage source={require("../../../image/logoEmpire.png")} />
+    )
+}
---
+++ src/components/atoms/Logo/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+
+export const StyledImage = styled.Image`
+  height: 110px;
+  width: 110px;
+`;
